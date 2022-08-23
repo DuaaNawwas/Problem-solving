cf84ec0f84e55af4b391bce019eb1cafc55dd41f
DuaaNawwas <duaa.nawwas@gmail.com>
2022-08-23 12:49:45 +0300

    added all questions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Problem-solving
+
+### webpage linked to app2.js
+
+[Live URL](https://duaanawwas.github.io/Problem-solving/)
+
+### app.js
+
+prompt and alert exercises
+
+### app2.js
+
+array methods exercises from 23/8/2022 and 16/8/2022 and regular expressions exercises
--- app2.js
+++ app2.js
@@ -12,7 +12,7 @@
 
 // This function should use forEach to populate your grocery list based on the store's inventory.
 //  If the item is available, add it to your list. Return the final list.
-console.log("----------app2.js----------------");
+console.log("----------app2.js// new questions 23/8/2022----------------");
 console.log("----------Q1----------------");
 let inventory = [
 	{ name: "apples", available: true },
@@ -120,3 +120,124 @@ function matchMonth() {
 	console.log(regex1.test(answer));
 }
 matchMonth();
+
+console.log("-----------old questions 16/8/2022 ------------");
+
+// Write a function named fizzbuzz that takes in an array of numbers.
+// Iterate over the array using forEach to determine the output based on several rules:
+//   - If a number is divisible by 3, add the word "Fizz" to the output array.
+//   - If the number is divisible by 5, add the word "Buzz" to the output array.
+//   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
+//   - Otherwise, add the number to the output array.
+// Return the resulting output array.
+console.log("-----------Q1 fizzbuzz ------------");
+let x = [4, 3, 5, 2, 6, 1, 9, 15];
+console.log(x.length);
+let output = [];
+console.log("-using for loop -");
+function fizzbuzz(x) {
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] % 3 == 0) {
+			output.push("Fizz");
+			if (x[i] % 3 == 0 && x[i] % 5 == 0) {
+				output.push("FizzBuzz");
+			}
+		} else if (x[i] % 5 == 0) {
+			output.push("Buzz");
+		} else {
+			output.push(x[i]);
+		}
+	}
+	return output;
+}
+
+console.log(fizzbuzz(x));
+
+console.log("-using forEach -");
+let output2 = [];
+x.forEach((x) => {
+	if (x % 3 == 0) {
+		output2.push("Fizz");
+		if (x % 3 == 0 && x % 5 == 0) {
+			output2.push("FizzBuzz");
+		}
+	} else if (x % 5 == 0) {
+		output2.push("Buzz");
+	} else {
+		output2.push(x);
+	}
+	return output2;
+});
+
+console.log(output2);
+
+// Write a function named forLoopTwoToThe that, given an array of integers as input, iterates over the array and returns a new array. The returned array should contain the result of raising 2 to the power of the original input element.
+
+// For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
+
+console.log("-----------Q2 two to the ------------");
+
+console.log("-using for loop -");
+let result = [];
+function forLoopTwoToThe(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		result.push(2 ** arr[i]);
+	}
+	return result;
+}
+console.log(forLoopTwoToThe([1, 2, 3]));
+
+console.log("-using map -");
+
+// let arr = [1, 2, 3];
+// let answer = arr.map((num) => 2 ** num);
+// console.log(answer);
+
+function toTheTwo1(arr) {
+	return arr.map((num) => {
+		return 2 ** num;
+	});
+}
+console.log(toTheTwo1([1, 2, 3]));
+// Write a function named oddValues that, given an array of integers as input, uses filter to return an array containing only the odd integers.
+
+console.log("-----------Q3 odd and even ------------");
+
+console.log("-using for loop -");
+
+let output1 = [];
+function oddValues(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 != 0) {
+			output.push(arr[i]);
+		}
+	}
+	return output1;
+}
+console.log(oddValues([1, 2, 3, 4, 5]));
+
+console.log("-using filter -");
+function oddValues(arr) {
+	return arr.filter((num) => num % 2 != 0);
+}
+console.log(oddValues([1, 2, 3, 4, 5, 6]));
+
+// Write a function named countNumberOfElements that, given an array as input, uses reduce to count the number of elements in the array.
+// Note: You may not use the array's built-in length property.
+console.log("-----------Q4 count numbers ------------");
+
+console.log("-using for loop -");
+let count = 0;
+function countNumberOfElements(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		count++;
+	}
+	return count;
+}
+console.log(countNumberOfElements([1, 2, 3, 4, 5, 6]));
+
+console.log("-using reduce -");
+function countNumberOfElements(arr) {
+	return arr.reduce((acc, num) => acc + 1, 0);
+}
+console.log(countNumberOfElements([1, 2, 3, 4, 5, 6]));
